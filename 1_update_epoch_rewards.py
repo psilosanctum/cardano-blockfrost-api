b4de489f9d786615206a9044a5b7817bc8225672
psilosanctum <psilosanctum@gmail.com>
2021-10-23 00:02:35 -0500

    add comments to convert epoch data

--- 1_update_epoch_rewards.py
+++ 1_update_epoch_rewards.py
@@ -2,4 +2,4 @@ import functions.a_authenticate_rewards_history
 import functions.b_total_staking_rewards_grouping
 import functions.c_rewards_converted_to_dollars
 import functions.d_epoch_data
-import functions.h_convert_epoch_data
+import functions.e_convert_epoch_data
--- functions/e_convert_epoch_data.py
+++ functions/e_convert_epoch_data.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
+# Create dataframe.
 path = "epoch_data/epoch_data.csv"
-
 df = pd.read_csv(path, index_col=[0])
+
+# Convert from lovelaces unit.
 df["output"] = df["output"]/1000000
 df["fees"] = df["fees"]/1000000
 df["active_stake"] = df["active_stake"]/1000000
+
+# Calculate average transaction cost.
 df["avg_transaction_cost"] = df["fees"]/df["tx_count"]
 print(df)
 
